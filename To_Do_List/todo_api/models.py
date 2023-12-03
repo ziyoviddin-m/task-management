36c6f08c1ef4bddedb8bd2880d090767ec20a990
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2023-12-03 20:13:34 +0400

    Added the ability to attach files to tasks

--- To_Do_List/todo_api/models.py
+++ To_Do_List/todo_api/models.py
@@ -5,6 +5,9 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
+    attachments = models.FileField(upload_to='task_attachments', blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+
