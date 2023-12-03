e884e67d811c0cb7fb529febe6024c452752f131
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2023-12-04 00:23:03 +0400

    Create filter, search and ordernig

--- To_Do_List/To_Do_List/settings.py
+++ To_Do_List/To_Do_List/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_filters',
     'rest_framework',
 
     'todo_api',
@@ -125,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
--- To_Do_List/todo_api/views.py
+++ To_Do_List/todo_api/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TaskSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Task
 
@@ -10,7 +10,10 @@ from .models import Task
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category']
+    search_fields = ['title', 'description']
+    ordering_fields = ['creation_date', 'title']
 
 # class TaskApiList(generics.ListCreateAPIView):
 #     queryset = Task.objects.all()
